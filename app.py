57bc18b8e54d79af6dd75249bdf4c6d73d1220fb
mashudalihassan <mashudalihassan@gmail.com>
2025-07-16 10:23:00 +0100

    Fix: updated app.py for Render compatibility

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-
-from flask import Flask, request, Response
-from twilio.twiml.voice_response import VoiceResponse
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/voice", methods=["POST"])
-def voice():
-    response = VoiceResponse()
-    response.play("https://drive.google.com/uc?export=download&id=1oXnHg7dLy-2DkKC80GyoMtcL79JTcpLy")
-    return Response(str(response), mimetype="application/xml")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/")
+def home():
+    return "Hello from Render!"
