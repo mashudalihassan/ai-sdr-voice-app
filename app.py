cbf77a4da7e8075ae20ece38a43065d76eb37064
mashudalihassan <mashudalihassan@gmail.com>
2025-07-16 11:48:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     response = VoiceResponse()
-    response.play("https://drive.google.com/uc?export=download&id=1oXnHg7dLy-2DkKC80GyoMtcL79JTcpLy")
+    response.play("https://ai-sdr-assets-4608.twil.io/voice_preview_british%20sdr.mp3")
     return Response(str(response), mimetype='text/xml')
 
 if __name__ == "__main__":
